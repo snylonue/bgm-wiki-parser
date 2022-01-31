b03d6b7b04bf0a632d205eb1cdb5efdf39d51491
snylonue <snylonue@gmail.com>
2022-01-31 12:21:16 +0800

    lib: implement some methods

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,39 @@ pub struct Wiki {
     pub kind: String,
     pub data: HashMap<String, Data>,
 }
+
+impl Item {
+    pub fn value(&self) -> Option<&str> {
+        match self {
+            Self::Single(v) | Self::Named(_, v) => Some(v),
+            Self::None => None,
+        }
+    }
+    pub fn value_mut(&mut self) -> Option<&mut str> {
+        match self {
+            Self::Single(v) | Self::Named(_, v) => Some(v),
+            Self::None => None,
+        }
+    }
+}
+
+impl Data {
+    pub fn is_array(&self) -> bool {
+        matches!(self, Self::Array(_))
+    }
+    pub fn is_scalar(&self) -> bool {
+        matches!(self, Self::Scalar(_))
+    }
+}
+
+impl Wiki {
+    pub fn new(kind: String, data: HashMap<String, Data>) -> Self {
+        Self { kind, data }
+    }
+    pub fn get(&self, field: &str) -> Option<&Data> {
+        self.data.get(field)
+    }
+    pub fn get_mut(&mut self, field: &str) -> Option<&mut Data> {
+        self.data.get_mut(field)
+    }
+}
